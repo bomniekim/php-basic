a4c2fa90fc220a49f151bbcb9d79ba58218fa8b8
Bomin Kim <bomnie.kim@gmail.com>
2020-01-03 17:25:25 +0900

    first php

---
+++ 01_hello.php
@@ -0,0 +1,23 @@
+<?php
+
+    echo "Hello World!";
+
+?>
+
+
+
+<?php
+
+    echo "Second php";
+    echo "<br>";
+    echo "next..";
+
+
+?>
+
+Nice to meet you 
+<!-- php 블럭 밖에 작성해도 그대로 출력됨 -->
+
+<hr>
+<h2>This is heading element</h2>
+<p>이것은 단락요소입니다.</p>
